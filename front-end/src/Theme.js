ec593ac9eb051c89fbf1f52e2272045330a2dda9
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-01 09:34:04 -0800

    Create theme palette using provided brand colors

---
+++ front-end/src/Theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1E9AAA'
+    },
+    secondary: {
+      main: '#fdb000'
+    },
+    tertiary: {
+      main: '#06d6a0'
+    },
+    black: {
+      main: '#4e4e4e'
+    },
+    white: {
+      main: '#ffffff'
+    },
+  }
+});
+
+export default theme;
