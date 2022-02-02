df86b70c7e86f4621df18a58e4b1da3c03c29cda
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-02 15:47:45 -0800

    Set card color

--- front-end/src/Components/SiteCard.jsx
+++ front-end/src/Components/SiteCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Card, CardHeader, Avatar } from '@mui/material';
 
-const SiteCard = ({ site }) => {
+const SiteCard = ({ site, color, index}) => {
   const siteName = site.name || site.displayName;
+  const colors = ['#1E9AAA', '#fdb000', '#06d6a0'];
+  const randomColor = colors[Math.floor(Math.random()*3)]
 
   return (
     <Card sx={{ maxWidth: 345 }}>
@@ -13,7 +15,7 @@ const SiteCard = ({ site }) => {
           </Avatar>
         }
         title={ siteName }
-        color='primary'
+        style={{backgroundColor: randomColor }}
         subheader={ site.id }
       />
     </Card>
