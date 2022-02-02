a89326898ac42eaef38a1cdc0014d110a814ae1d
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-02 11:55:49 -0800

    Use drawer for layout instead of Navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-// import logo from './logo.svg';
-import NavBar from './Components/Navbar';
+import Layout from './Components/Drawer';
 import data from './data.json'
 import './App.css';
 import theme from './Theme'
 import { ThemeProvider } from '@mui/material/styles';
 
-
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={ theme }>
-        <NavBar organization={ data }/>
+        <Layout organization={ data } />
       </ThemeProvider>
     </div>
   );
