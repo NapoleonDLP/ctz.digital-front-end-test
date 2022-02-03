f26379a3f3a3203e9fc0714b3322f795e7e45943
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-02 23:26:48 -0800

    Add card actions and expandable collapse

--- front-end/src/Components/SiteCard.jsx
+++ front-end/src/Components/SiteCard.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Card, CardHeader, Avatar, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardHeader, CardActions, Collapse, CardContent, Avatar, Typography, IconButton } from '@mui/material';
 import './SiteCard.css';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { styled } from '@mui/material/styles';
+
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const SiteCard = ({ site, color, index}) => {
   const siteName = site.name || site.displayName;
   const colors = ['#1E9AAA', '#fdb000', '#06d6a0'];
   const randomColor = colors[Math.floor(Math.random()*3)]
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <Card sx={{ maxWidth: 345, borderRadius: 3 }} raised={ true }>
@@ -30,6 +49,23 @@ const SiteCard = ({ site, color, index}) => {
           </Typography>
         }
       />
+
+      <CardActions>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+
+      <Collapse in={ expanded } timeout='auto' unmountOnExit>
+        <CardContent>
+
+        </CardContent>
+      </Collapse>
     </Card>
   )
 };
