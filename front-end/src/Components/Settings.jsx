b1df631321286694429c29184f1d4680963de9eb
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-03 14:37:08 -0800

    Add settings navigation list

--- front-end/src/Components/Settings.jsx
+++ front-end/src/Components/Settings.jsx
@@ -1,16 +1,72 @@
 import React from 'react';
 import NavBar from './Navbar';
+
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import SettingsIcon from '@mui/icons-material/Settings';
+import InsightsIcon from '@mui/icons-material/Insights';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+
 import './Settings.css';
 
-const Settings = ({ organization }) => {
+const Settings = ({ organization, position }) => {
 
   return (
     <div className='settingsContainer'>
       <div>
-        <NavBar organization={ organization } className='settingsNavbar'/>
+        <NavBar position='static' organization={ organization } className='settingsNavbar'/>
       </div>
       <div>
-        <h1 className='settingsBox'>SETTINGS PAGE</h1>
+        <Box sx={{ width: '100%', maxWidth: 200, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="General" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InsightsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Revenue" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InsertDriveFileIcon />
+              </ListItemIcon>
+              <ListItemText primary="Document" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Trash" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
       </div>
     </div>
   )
