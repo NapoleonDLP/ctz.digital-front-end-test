5d37a46b15742d8d41ccc3de9807a796ca036962
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-04 03:24:09 -0800

    Show productTypes for each site

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import './ProductList.css';
 
 const ProductList = ({ product }) => {
-  console.log(product)
+  const availableProducts = {
+    "Interstitial": null,
+    "Leave-Behind": 'https://static.clicktripz.com/home/img/icon-compare.png',
+    "Parallax": 'https://static.clicktripz.com/home/img/icon-parallax.png',
+    "Overlay": 'https://static.clicktripz.com/home/img/icon-overlay.png',
+  };
+
   return (
     <div className='product'>
       <Avatar variant="square">
-
+        <img className='productImage' src={ availableProducts[product.type] } alt='I'/>
       </Avatar>
       <div className='productType'>
         <Typography>
           { product.type }
         </Typography>
       </div>
-
-
     </div>
   )
 };
