d6d173492535f41fe2693dbbed8f90da0586e6be
NapoleonDLP <napoleon.dlp@gmail.com>
2022-02-03 20:23:27 -0800

    Add enable and test publishe switches

--- front-end/src/Components/GeneralSettings.jsx
+++ front-end/src/Components/GeneralSettings.jsx
@@ -1,68 +1,41 @@
 import React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import Switch from '@mui/material/Switch';
-import WifiIcon from '@mui/icons-material/Wifi';
-import BluetoothIcon from '@mui/icons-material/Bluetooth';
 
 const GeneralSettings = ({ settings }) => {
-  const [checked, setChecked] = React.useState(['wifi']);
-  // const [enabled, setEnabled] = React.useState(settings.enable);
-  // const [testPublisher, setTestPublisher] = React.useState(settings.testPublisher);
+  const [enabled, setEnabled] = React.useState(settings.enabled);
+  const [testPublisher, setTestPublisher] = React.useState(settings.testPublisher);
 
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-  //General settings have
-    //enabled: bool
-    //testPublisher: bool
     //timezone: string
     //accountType: string
 
-  // Create a list to show with toggles
+  const handleToggle = (value) => () => {
+    if (value === 'enabled') {
+      setEnabled(!enabled);
+    } else if (value === 'testPublisher') {
+      setTestPublisher(!testPublisher);
+    }
+  };
+
   return (
     <List
-      sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }}
+      sx={{ width: 400, maxWidth: 500, bgcolor: 'background.paper' }}
       subheader={<ListSubheader>Settings</ListSubheader>}
     >
-      <ListItem>
-        <ListItemIcon>
-          <WifiIcon />
-        </ListItemIcon>
-        <ListItemText id="switch-list-label-wifi" primary="Wi-Fi" />
+      <ListItem justifyContent='space-between'>
+        <ListItemText primary="Enable" />
         <Switch
-          edge="end"
-          onChange={handleToggle('wifi')}
-          checked={checked.indexOf('wifi') !== -1}
-          inputProps={{
-            'aria-labelledby': 'switch-list-label-wifi',
-          }}
+          onChange={ handleToggle('enabled') }
         />
       </ListItem>
       <ListItem>
-        <ListItemIcon>
-          <BluetoothIcon />
-        </ListItemIcon>
-        <ListItemText id="switch-list-label-bluetooth" primary="Bluetooth" />
+        <ListItemText primary="Test Publisher" />
         <Switch
-          edge="end"
-          onChange={handleToggle('bluetooth')}
-          checked={checked.indexOf('bluetooth') !== -1}
-          inputProps={{
-            'aria-labelledby': 'switch-list-label-bluetooth',
-          }}
+          onChange={ handleToggle('testPublisher') }
         />
       </ListItem>
     </List>
